d346fe6483a5a8719077122691c893269730aedf
Harry Chopra <harrychopra@gmail.com>
2021-08-06 23:50:57 +0100

    user controller

---
+++ controllers/user_controller.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/harrychopra/go-mvc/services"
+	"github.com/harrychopra/go-mvc/utils"
+)
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	userIdParam := r.URL.Query().Get("user_id")
+	userId, err := strconv.ParseInt(userIdParam, 10, 64)
+	if err != nil {
+		apiErr := &utils.ApplicationError{
+			Message:    "user_id must be a number",
+			StatusCode: http.StatusBadRequest,
+			Code:       "bad request",
+		}
+		bApiErr, _ := json.Marshal(apiErr)
+		w.WriteHeader(apiErr.StatusCode)
+		w.Write(bApiErr)
+		return
+	}
+	user, apiErr := services.GetUser(userId)
+	if apiErr != nil {
+		bApiErr, _ := json.Marshal(apiErr)
+		w.WriteHeader(apiErr.StatusCode)
+		w.Write(bApiErr)
+		return
+	}
+	bUser, _ := json.Marshal(user)
+	w.WriteHeader(http.StatusOK)
+	w.Write(bUser)
+}
